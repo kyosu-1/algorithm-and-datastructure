cb648fce6a8df7dc2d76f651e4b03bcd45d3eb14
kyosu-1 <sho013039@gmail.com>
2023-01-09 17:24:32 +0900

    add bfs

--- graph/bfs.go
+++ graph/bfs.go
@@ -1,1 +1,25 @@
 package graph
+
+import (
+	"github.com/kyosu-1/algorithm-and-datastructure/datastructure/queue"
+)
+
+// BreadthFirstSearch performs a breadth-first search of the graph g, starting at
+// the given source vertex, and calls the function f for each vertex visited.
+// use queue data structure
+func BreadthFirstSearch(g Graph, s int, f func(int)) {
+	visited := make([]bool, g.Order())
+	visited[s] = true
+	q := queue.New[int]()
+	q.Enqueue(s)
+	for !q.IsEmpty() {
+		v := q.Dequeue()
+		f(v)
+		for _, w := range g.Adjacent(v) {
+			if !visited[w] {
+				visited[w] = true
+				q.Enqueue(w)
+			}
+		}
+	}
+}
